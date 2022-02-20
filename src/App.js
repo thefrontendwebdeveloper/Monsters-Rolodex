77eb36a8ac89739aa1b8e9114c5e76337a40084a
thefrontendwebdeveloper <himangshu.web.design@gmail.com>
2022-02-20 16:15:37 +0530

    learning react

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+import { SearchBox } from './component/search-box/search-box';
+
+import './App.css';
+
+import { CardList } from './component/card-list/card-list.jsx'
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+
+      const { monsters, searchField } = this.state;
+      const filteredMonsters = monsters.filter(monster => 
+       monster.name.toLowerCase().includes(searchField.toLowerCase()) 
+      );
+   
+
+      return (
+        <div className="App">
+          <h1>Monsters Rolodex</h1>
+          <SearchBox 
+            placeholder='search monsters'
+            handleChange={this.handleChange}
+          />
+          <CardList monsters={filteredMonsters} />
+        </div>
+      );
+    }
+  }
+export default App;
